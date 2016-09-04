68f40c98a7bc0a88a465c1a5f177e45a955f7023
Erkan Sen <erkansen14@hotmail.com>
2016-09-04 01:08:01 -0500

    added prototypes for Pokemon and Move

--- Main.js
+++ Main.js
@@ -1,14 +1,14 @@
 var readline = require('readline')
 var Prototypes = require('./Prototypes')
 
-var Player = Prototypes.Player
-var Player2 = Prototypes.Player
 
+var Player1 = Prototypes.Player
+var Player2 = Prototypes.Player
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 rl.question("What is your name? ", function(answer) {
-    Player.name = answer
-    console.log('Welcome to the battle, ' + Player.name + '!' )
+    Player1.name = answer
+    console.log('Welcome to the battle, ' + Player1.name + '!' )
     rl.close()
     MenuPhase();
 });
@@ -33,5 +33,5 @@ function SwitchPokemon() {
 }
 
 function Items() {
-  
+
 }
--- Prototypes.js
+++ Prototypes.js
@@ -1,3 +1,18 @@
-module.exports.Player = new function(name) {
-  this.name = name;
+module.exports.Player = new function() {
+  this.name;
+  this.pokemon;
+  this.items;
+}
+
+module.exports.Pokemon = new function() {
+  this.moves;
+  this.atk;
+  this.hp;
+  this.def;
+  this.dodge;
+}
+
+module.exports.Move = new function() {
+  this.damage;
+  this.name;
 }
